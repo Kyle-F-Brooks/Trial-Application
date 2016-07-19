725d45b05424fdd7bca5cb75b06cf8951fa6caca
Evan Harry <eharry93@icloud.com>
2016-07-19 21:30:16 +0100

    Add unit tests for Actions class

---
+++ Trial ApplicationTests/ActionsTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Trial_Application;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trial_Application.Tests
+{
+    [TestClass()]
+    public class ActionsTests
+    {
+        [TestMethod()]
+        public void SayHelloTest()
+        {
+            var expected = "Hello";
+            Assert.AreEqual(Actions.SayHello(), expected);
+        }
+
+        [TestMethod()]
+        public void SayGoodbyeTest()
+        {
+            var expected = "Goodbye";
+            Assert.AreEqual(Actions.SayGoodbye(), expected);
+        }
+    }
+}
